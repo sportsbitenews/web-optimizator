fe522d88de8e2b2326c04768d446b41b7d599229
sunny.dreamwind@gmail.com <sunny.dreamwind@gmail.com@59515e40-0e14-11de-b8f3-b9fb9eb71b04>
2009-04-13 12:26:44 +0000

    Uninstall page layout separated from stage1

---
+++ view/install_uninstall.php
@@ -0,0 +1,9 @@
+<?php if(!empty($message)) { ?><div class="success"><?php echo $message ?></div><?php } ?>
+
+<h1><?php echo _WEBO_SPLASH1_UNINSTALL ?></h1>
+
+<p><?php echo _WEBO_SPLASH1_UNINSTALL_THANKS . $_SERVER['HTTP_HOST'] . $paths['relative']['current_directory'] . _WEBO_SPLASH1_UNINSTALL_THANKS2 ?></p>
+
+<p><?php echo _WEBO_SPLASH1_UNINSTALL_VISIT ?></p>
+
+<p><?php echo _WEBO_SPLASH1_UNINSTALL_BACK . $paths['relative']['document_root'] . _WEBO_SPLASH1_UNINSTALL_BACK2 ?></p>
