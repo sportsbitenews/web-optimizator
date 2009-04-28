62dd5ca3c743ce9443b3234a6ab923b05e95a2dd
sunny.dreamwind@gmail.com <sunny.dreamwind@gmail.com@59515e40-0e14-11de-b8f3-b9fb9eb71b04>
2009-04-28 15:42:46 +0000

    Minor bug with curly brackets fixed

--- controller/compressor.php
+++ controller/compressor.php
@@ -774,7 +774,7 @@ class web_optimizer {
 							$variant_type[1] = strtolower($variant_type[1]);
 							switch ($variant_type[1]) {
 								case "src":
-									$file['file'] = preg_replace("/(\?|#).*/", "", $variant_type[2];
+									$file['file'] = preg_replace("/(\?|#).*/", "", $variant_type[2]);
 									break;
 								default:
 									$file[$variant_type[1]] = $variant_type[2];
@@ -802,7 +802,7 @@ class web_optimizer {
 							$variant_type[1] = strtolower($variant_type[1]);
 							switch ($variant_type[1]) {
 								case "href":
-									$file['file'] = preg_replace("/(\?|#).*/", "", $variant_type[2];
+									$file['file'] = preg_replace("/(\?|#).*/", "", $variant_type[2]);
 									break;
 								default:
 /* skip media="all" to prevent Safari bug with @media all{} */
