2539a7f52a4459fd514366e63da90d04936dbbc8
sunny.dreamwind <sunny.dreamwind@59515e40-0e14-11de-b8f3-b9fb9eb71b04>
2009-10-14 22:34:31 +0000

    Disabled separated CSS file load on DOMready -- need to perform complete tests

--- config.webo.php
+++ config.webo.php
@@ -109,7 +109,7 @@ $compress_options['data_uris']['on'] = "1";
 ## Should Web Optimizer separate CSS for rules and images?
 $compress_options['data_uris']['separate'] = "1";
 ## Should Web Optimizer load resource CSS on DOMloaded event?
-$compress_options['data_uris']['domloaded'] = "1";
+$compress_options['data_uris']['domloaded'] = "0";
 ## Optimiza all CSS images via smush.it?
 $compress_options['data_uris']['smushit'] = "0";
 ## Maximum size of images to be converted, in bytes
