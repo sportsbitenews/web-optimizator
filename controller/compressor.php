b79f4f4a09432759edf8f287ade2ed502f890606
sunny.dreamwind <sunny.dreamwind@59515e40-0e14-11de-b8f3-b9fb9eb71b04>
2010-07-30 12:32:41 +0000

    Fixes a bit CSS minify

--- controller/compressor.php
+++ controller/compressor.php
@@ -2247,7 +2247,7 @@ class web_optimizer {
 		$txt = str_replace(array(' :', ': ', ' ,', ', ', ' ;', '; ', ' {', '{ ', ' }', '} '), array(':', ':', ',', ',', ';', ';', '{', '{', '}', '}'), $txt);
 /* Remove excessive symbols */
 		$txt = str_replace(array(' 0px', ':0px', ';}', ':0 0 0 0', ':0.', ' 0.'), array(' 0', ':0', '}', ':0', ':.', ' .'), $txt);
-		return $txt;
+		return trim($txt);
 	}
 
 	/**
