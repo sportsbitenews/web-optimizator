4e407944e5afe9655eb84d34761bae2ce7fba695
sunny.dreamwind@gmail.com <sunny.dreamwind@gmail.com@59515e40-0e14-11de-b8f3-b9fb9eb71b04>
2010-12-02 09:25:16 +0000

    Fixed issue with rel="stylesheet nofollow" in IE7-

--- controller/compressor.php
+++ controller/compressor.php
@@ -1687,8 +1687,12 @@ class web_optimizer {
 	function get_new_file ($options, $cache_file, $timestamp = false, $add = false) {
 		$newfile = '<' . $options['tag'] .
 			' type="' . $options['type'] . '" ' .
-			$options['src'] . '="' . $this->get_new_file_name($options, $cache_file, $timestamp, $add) . '" rel="' .
-			(empty($options['rel'])? '' : $options['rel'] . ' ') . 'nofollow"' . 
+			$options['src'] . '="' . $this->get_new_file_name($options, $cache_file, $timestamp, $add) . '"'.
+/* IE7- don't understand stylesheet nofollow in rel */
+			(empty($this->ua_mod) || !empty($options['rel']) ? 'rel="' .
+				(empty($options['rel']) ? '' : $options['rel'] . ' ') .
+				(!empty($options['rel']) && empty($this->ua_mod) ? ' ' : '') .
+				(empty($this->ua_mod) ? 'nofollow' : '') . '"' : '') . 
 			(empty($options['self_close']) ? '></' . $options['tag'] . '>' : (empty($this->xhtml) ? '>' : '/>'));
 		return $newfile;
 	}
