0d7495c65dcaa9005b7f586e5bc269a390473332
sunny.dreamwind <sunny.dreamwind@59515e40-0e14-11de-b8f3-b9fb9eb71b04>
2011-02-21 13:47:20 +0000

    Added support for 1-string WEBO Site SpeedUp enabling: $webo_not_buffered=1;require('../web.optimizer.php'); in the very beginning of index.php

--- web.optimizer.php
+++ web.optimizer.php
@@ -3,52 +3,67 @@
 // Licensed under the WEBO license (LICENSE.txt)
 // ==============================================================================================
 // @author     WEBO Software (http://www.webogroup.com/)
-// @version    1.0.0
-// @copyright  Copyright &copy; 2009-2010 WEBO Software. All Rights Reserved
+// @version    1.4.0
+// @copyright  Copyright &copy; 2009-2011 WEBO Software. All Rights Reserved
 // ==============================================================================================
 
-$basepath = realpath(dirname(__FILE__)) . '/';
-$compress_options['php'] = substr(phpversion(), 0, 1);
-
-if (!class_exists('compressor', false)) {
-	require_once($basepath . "controller/compressor.php");
+function finish_webositespeedup ($content) {
+    if ($content) {
+        $not_buffered = 1;
+        require(realpath(dirname(__FILE__)) . '/web-optimizer/web.optimizer.php');
+        if (!empty($web_optimizer)) {
+            $webo_content = $web_optimizer->finish($content);
+        }
+        if (!empty($webo_content)) {
+            $content = $webo_content;
+        }
+        return $content;
+    }
 }
+
+/* to include WEBO Site SpeedUp in 1-string mode to CMS */
+if (!empty($webo_not_buffered)) {
+	ob_start('finish_webositespeedup');
+} else {
+/* defining constants */
+	$basepath = realpath(dirname(__FILE__)) . '/';
+	$compress_options['php'] = substr(phpversion(), 0, 1);
+/* main library */
+	if (!class_exists('compressor', false)) {
+		require_once($basepath . "controller/compressor.php");
+	}
 /* Include this for path getting help */
-if (!class_exists('compressor_view', false)) {
-	require_once($basepath . "libs/php/view.php");
-}
-
+	if (!class_exists('compressor_view', false)) {
+		require_once($basepath . "libs/php/view.php");
+	}
 /* We need to know the config */
-require($basepath . "config.webo.php");
-
+	require($basepath . "config.webo.php");
 /* buffer input stream or not */
-$compress_options['buffered'] = empty($not_buffered) ? 1 : 0;
-
+	$compress_options['buffered'] = empty($not_buffered) ? 1 : 0;
 /* Con. the view library */
-$view = new compressor_view();
-
+	$view = new compressor_view();
 /* create libraries array -- include them only if we are really compressing */
-$libraries = array();
-
+	$libraries = array();
 /* Include this for CSS Sprites generating */
-$libraries['css_sprites'] = 'css.sprites.php';
-$libraries['css_sprites_optimize'] = 'css.sprites.optimize.php';
+	$libraries['css_sprites'] = 'css.sprites.php';
+	$libraries['css_sprites_optimize'] = 'css.sprites.optimize.php';
 /* JSMin */
-$libraries['JSMin'] = 'jsmin5.php';
+	$libraries['JSMin'] = 'jsmin5.php';
 /* Dean Edwards Packer */
-$libraries['JavaScriptPacker'] = 'packer5.php';
+	$libraries['JavaScriptPacker'] = 'packer5.php';
 /* CSS Tidy */
-$libraries['csstidy'] = 'class.csstidy.php';
+	$libraries['csstidy'] = 'class.csstidy.php';
 /* YUI Compressor */
-$libraries['YuiCompressor'] = 'class.yuicompressor.php';
+	$libraries['YuiCompressor'] = 'class.yuicompressor.php';
 /* Google Compiler */
-$libraries['GoogleCompiler'] = 'class.googlecompiler.php';
+	$libraries['GoogleCompiler'] = 'class.googlecompiler.php';
 
 /* Con. the compression controller */
-$web_optimizer = new web_optimizer(array(
-	'view' => $view,
-	'options' => $compress_options,
-	'libraries' => $libraries,
-	'no_cache' => empty($no_cache) ? false : $no_cache)
-);
+	$web_optimizer = new web_optimizer(array(
+		'view' => $view,
+		'options' => $compress_options,
+		'libraries' => $libraries,
+		'no_cache' => empty($no_cache) ? false : $no_cache)
+	);
+}
 ?>
