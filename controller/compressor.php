4b2caaa8a8d58cc00cf7a7f92da0fbba35891b09
sunny.dreamwind <sunny.dreamwind@59515e40-0e14-11de-b8f3-b9fb9eb71b04>
2011-07-03 16:45:23 +0000

    Fixed invalid URLs parsing (excessive ../)

--- controller/compressor.php
+++ controller/compressor.php
@@ -2289,7 +2289,7 @@ class web_optimizer {
 					}
 					$content_from_file = '';
 					if (!empty($value['file'])) {
-						$value['file'] = preg_replace("@^(\.\./)+@", "", $value['file']);
+						$value['file'] = preg_replace("@^/?(\.\./)+@", "", $value['file']);
 /* convert dynamic files to static ones */
 						if (!preg_match("/\.(css|js)$/is", $value['file']) || strpos($value['file'], 'index.php/')) {
 							$dynamic_file = $value['file_raw'];
