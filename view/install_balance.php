59e6e2c2b271640a5ff3a5219c2c881e55272e44
sunny.dreamwind <sunny.dreamwind@59515e40-0e14-11de-b8f3-b9fb9eb71b04>
2012-01-16 09:02:12 +0000

    Fixed PHP notice on balance page

--- view/install_balance.php
+++ view/install_balance.php
@@ -75,7 +75,7 @@
 	foreach ($options as $key => $group) {
 		if (is_array($group)) {
 			foreach ($group as $option => $value) {
-				if (!empty($value['value'])) {
+				if (!empty($value['value']) && !empty($value['price'])) {
 					if ($price = is_array($value['price']) ? $value['price'][$value['value']] : $value['price']) {
 						$daily += empty($value['value']) ? 0 : $price;
 						$saas .= '<tr class="wssT8"><td class="wssT9">' .
