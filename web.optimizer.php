17fa71089228e674284a5e3e68ebfe4fba8b5ede
sunny.dreamwind <sunny.dreamwind@59515e40-0e14-11de-b8f3-b9fb9eb71b04>
2012-02-17 07:23:24 +0000

    Improved handling shutdown approach

--- web.optimizer.php
+++ web.optimizer.php
@@ -7,6 +7,9 @@
 // @copyright  Copyright &copy; 2009-2012 WEBO Software. All Rights Reserved
 // ==============================================================================================
 
+$no_cache = isset($_SESSION['log']) ? 1 : 0;
+$webo_request_uri = $_SERVER['REQUEST_URI'];
+
 /* Test failed store to this file. 2 steps: check and clean content if not valid */
 if (!function_exists('webo_integrity_handler')) {
 
