6d04f487b438460bb45c0677a2dc213801ae5d1c
sunny.dreamwind <sunny.dreamwind@59515e40-0e14-11de-b8f3-b9fb9eb71b04>
2012-04-06 13:18:53 +0000

    Fixed notice for RU locale

--- libs/php/lang/ru.php
+++ libs/php/lang/ru.php
@@ -905,7 +905,6 @@ define('_WEBO_SPLASH2_COMPARISON_WEEKS', 'недели');
 define('_WEBO_SPLASH2_COMPARISON_MONTHS', 'месяцев');
 define('_WEBO_SPLASH2_COMPARISON_LICENSING', 'Срок действия лицензии');
 define('_WEBO_SPLASH2_COMPARISON_UNLIMITED', 'Не ограничен');
-define('_WEBO_SPLASH2_COMPARISON', 'Купить ускорение сайта');
 define('_WEBO_SPLASH2_COMPARISON_MORE', 'Для профессионального ускорения сайта больше подойдет');
 define('_WEBO_SPLASH2_COMPARISON_LIVE', 'Рабочий режим');
 
