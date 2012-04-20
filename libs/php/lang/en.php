7ae6b2d0b1c93cf74007ab7c5de1784679ca2e1e
sunny.dreamwind <sunny.dreamwind@59515e40-0e14-11de-b8f3-b9fb9eb71b04>
2012-04-20 14:36:47 +0000

    Fixed texts in admin UI about CSS Sprites disabled

--- libs/php/lang/en.php
+++ libs/php/lang/en.php
@@ -473,7 +473,7 @@ define('_WEBO_css_sprites_dimensions_limited_HELP', 'Images higher or wider than
 define('_WEBO_css_sprites_dimensions_limited_HELP_DISABLED', 'There is no complete GD library support on your website, so CSS Sprites creation is impossible. Or CSS Tidy library isn\'t selected to minify CSS.');
 define('_WEBO_css_sprites_sprites_limited', 'Maximum width and height of final sprites (in pixels)');
 define('_WEBO_css_sprites_sprites_limited_HELP', 'Sprites\' dimensions will be restricted to defined value. No value or zero value means no restriction.');
-define('_WEBO_css_sprites_sprites_limited_HELP_DISABLED', 'There is no complete GD library support on your website, so CSS Sprites creation is impossible. Or CSS Tidy library isn\'t selected to minify CSS.');
+define('_WEBO_css_sprites_sprites_limited_HELP_DISABLED', 'There is no complete GD library support on your website, so CSS Sprites creation is impossible.');
 define('_WEBO_css_sprites_ignore', 'Exclude / include files for CSS Sprites');
 define('_WEBO_css_sprites_ignore_HELP', 'All images listed below either won\'t included into CSS Sprites, or there will be included only these images.');
 define('_WEBO_css_sprites_ignore_HELP_DISABLED', 'There is no complete GD library support on your website, so CSS Sprites creation is impossible. Or CSS Tidy library isn\'t selected to minify CSS.');
