fd6cd4c12305ee87b086bd326e669ed8c766e5f7
sunny.dreamwind@gmail.com <sunny.dreamwind@gmail.com@59515e40-0e14-11de-b8f3-b9fb9eb71b04>
2012-10-09 16:14:09 +0000

    Added marva widget to unobtrusive logic (3)

--- libs/php/config.unobtrusive.php
+++ libs/php/config.unobtrusive.php
@@ -166,7 +166,7 @@ $unobtrusive_items = array(
 		), 'mr' => array(
 			'marker' => 'marva',
 			'regexp' => '<script[^>]*>//<!\[CDATA\[[^>]*?marva[^>]*?//\]\]></script>',
-			'onload_before' => '<script[^>]*>//<!\[CDATA\[([^>]*?marva[^>]*?)//\]\]></script>',
+			'onload_before' => '<script[^>]*>[\r\n]?//<!\[CDATA\[([^>]*?marva[^>]*?)//\]\]>[\r\n]?</script>',
 			'onload_after' => '$1;setTimeout(function(){if(typeof window.marva!=="undefined"){wss_onload_ready=1}else{setTimeout(arguments.callee,10)}},10)'
 /* Odnaknopka */
 		), 'ok' => array(
